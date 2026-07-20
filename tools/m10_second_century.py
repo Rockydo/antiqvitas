f7b008b80736653431ef981aba075e39f56de095
Rockydo <alvin.harberts@gmail.com>
2026-07-20 11:30:41 +0200

    feat(m11): illustrate Gothic migration

--- tools/m10_second_century.py
+++ tools/m10_second_century.py
@@ -63,6 +63,7 @@ EVENT_IMAGES = {
     "cai_lun_paper": "gfx/interface/illustrations/event/antq_cai_lun_paper.dds",
     "celestial_masters": "gfx/interface/illustrations/event/antq_celestial_masters.dds",
     "gan_ying": "gfx/interface/illustrations/event/antq_gan_ying.dds",
+    "gothic_migration": "gfx/interface/illustrations/event/antq_gothic_migration.dds",
     "hadrians_wall": "gfx/interface/illustrations/event/antq_hadrians_wall.dds",
     "kanishka_apogee": "gfx/interface/illustrations/event/antq_kanishka_apogee.dds",
     "trajan_dacia": "gfx/interface/illustrations/event/antq_trajan_dacia.dds",
