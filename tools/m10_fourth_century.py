ea873c9fde525ee84f23fefe965074f41665bfb1
Rockydo <alvin.harberts@gmail.com>
2026-07-20 15:25:28 +0200

    feat(m11): illustrate East-West division

--- tools/m10_fourth_century.py
+++ tools/m10_fourth_century.py
@@ -67,6 +67,7 @@ EVENT_IMAGES = {
     "armenia_conversion": "gfx/interface/illustrations/event/antq_armenia_conversion.dds",
     "constantine_civil_wars": "gfx/interface/illustrations/event/antq_constantine_civil_wars.dds",
     "crete_earthquake": "gfx/interface/illustrations/event/antq_crete_earthquake.dds",
+    "east_west_division": "gfx/interface/illustrations/event/antq_east_west_division.dds",
     "fei_river": "gfx/interface/illustrations/event/antq_fei_river.dds",
     "gothic_refugees": "gfx/interface/illustrations/event/antq_gothic_refugees.dds",
     "gwanggaeto": "gfx/interface/illustrations/event/antq_gwanggaeto.dds",
