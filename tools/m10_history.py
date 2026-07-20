929aefa6e39b3a0c0cc3c54fbb5e9f18a663e0f7
Rockydo <Rockydo@users.noreply.github.com>
2026-07-20 08:32:13 +0200

    feat(m11): illustrate Buddhism at Han court

--- tools/m10_history.py
+++ tools/m10_history.py
@@ -74,6 +74,7 @@ TARGETS = {
 EVENT_IMAGES = {
     "augustan_succession": "gfx/interface/illustrations/event/antq_augustan_succession.dds",
     "boudica_revolt": "gfx/interface/illustrations/event/antq_boudica_revolt.dds",
+    "buddhism_han_court": "gfx/interface/illustrations/event/antq_buddhism_han_court.dds",
     "christianity_foundation": "gfx/interface/illustrations/event/antq_christianity_foundation.dds",
     "claudian_britain": "gfx/interface/illustrations/event/antq_claudian_britain.dds",
     "florus_sacrovir": "gfx/interface/illustrations/event/antq_florus_sacrovir.dds",
