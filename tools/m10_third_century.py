737c97b8fb7d49d6efc48dbcf45faad1d1f413f5
Rockydo <alvin.harberts@gmail.com>
2026-07-20 12:52:05 +0200

    feat(m11): illustrate Three Kingdoms

--- tools/m10_third_century.py
+++ tools/m10_third_century.py
@@ -53,6 +53,7 @@ EVENT_IMAGES = {
     "alemanni_formation": "gfx/interface/illustrations/event/antq_alemanni_formation.dds",
     "constitutio_antoniniana": "gfx/interface/illustrations/event/antq_constitutio_antoniniana.dds",
     "severus_caledonia": "gfx/interface/illustrations/event/antq_severus_caledonia.dds",
+    "three_kingdoms": "gfx/interface/illustrations/event/antq_three_kingdoms.dds",
 }
 
 # These are visual country identities, intentionally not runtime country tags.
