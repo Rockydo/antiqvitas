113dbf66986cd18c79ac183c3e984653369615e3
Rockydo <Rockydo@users.noreply.github.com>
2026-07-20 07:15:53 +0200

    feat(m11): illustrate Illyrian revolt

--- tools/m10_history.py
+++ tools/m10_history.py
@@ -74,6 +74,7 @@ TARGETS = {
 EVENT_IMAGES = {
     "augustan_succession": "gfx/interface/illustrations/event/antq_augustan_succession.dds",
     "gaius_eastern_settlement": "gfx/interface/illustrations/event/antq_gaius_eastern_settlement.dds",
+    "illyrian_revolt": "gfx/interface/illustrations/event/antq_illyrian_revolt.dds",
     "immensum_bellum": "gfx/interface/illustrations/event/antq_immensum_bellum.dds",
     "wang_mang_xin": "gfx/interface/illustrations/event/antq_xin_dynasty_crisis.dds",
 }
