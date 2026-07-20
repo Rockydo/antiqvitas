cb41a3a8ed377a5ae7b0be764ccb3bbe65f5eb2a
Rockydo <Rockydo@users.noreply.github.com>
2026-07-20 17:01:36 +0200

    feat(m11): illustrate Chalcedon and Avarayr

--- tools/m10_final_century.py
+++ tools/m10_final_century.py
@@ -47,6 +47,7 @@ EVENT_IMAGES = {
     "adventus_saxonum": "gfx/interface/illustrations/event/antq_adventus_saxonum.dds",
     "attila": "gfx/interface/illustrations/event/antq_attila.dds",
     "britain_abandoned": "gfx/interface/illustrations/event/antq_britain_abandoned.dds",
+    "chalcedon_avarayr": "gfx/interface/illustrations/event/antq_chalcedon_avarayr.dds",
     "constantinople_earthquake": "gfx/interface/illustrations/event/antq_constantinople_earthquake.dds",
     "hephthalites": "gfx/interface/illustrations/event/antq_hephthalites.dds",
     "radagaisus_rhine": "gfx/interface/illustrations/event/antq_radagaisus_rhine.dds",
